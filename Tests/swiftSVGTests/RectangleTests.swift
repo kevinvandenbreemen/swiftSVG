15897343f28faf3ecdacde2c95aab4b00733a034
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-02-11 22:35:08 -0500

    Start work on Rectangle shape

---
+++ Tests/swiftSVGTests/RectangleTests.swift
@@ -0,0 +1,29 @@
+import XCTest
+
+import swiftSVG
+
+class RectangleTests: XCTestCase {
+
+    func testCreateRectangle() {
+
+        let svg = SVG(width: 100, height: 100)
+        let rect = Rectangle(x: 10, y: 20, width: 100, height: 200)
+
+        svg.add(rect)
+
+        let expected = 
+        """
+        <svg width="100" height="100">
+        \t<rect x="10" y="10" width="100" height="200" />
+        </svg>
+        """
+
+        XCTAssertEqual(svg.render(), expected)
+
+    }
+
+    static var allTests = [
+        "Create a Rectangle": testCreateRectangle
+    ]
+
+}
